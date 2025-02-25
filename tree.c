33ffd514af6ddf87df71cfa94d1b7dc78f31a682
ViorelsS <viorelss@icloud.com>
2025-02-25 15:00:36 +0100

    Refactor tree.c for improved readability and consistency

--- tree.c
+++ tree.c
@@ -1,27 +1,28 @@
 /* tree.c */
 #include "tree.h"
 
+Tree root = {.n = {
+                 .tag = (TagRoot | TagNode),
+                 .north = (Node *)&root,
+                 .west = 0,
+                 .east = 0,
+                 .path = "/"}};
 
-Tree root = { .n ={
-    .tag = (TagRoot | TagNode),
-    .north = (Node *)&root, 
-    .west = 0,
-    .east = 0,
-    .path = "/"
-}};
-
-void zero(int8 *str, int16 size){
+void zero(int8 *str, int16 size)
+{
     int8 *p;
     int16 n;
 
-    for(n=0, p=str; n<size ; p++, n++){
+    for (n = 0, p = str; n < size; p++, n++)
+    {
         *p = 0;
     }
 
     return;
 }
 
-Node *createNode(Node *parent, int8 *path){
+Node *createNode(Node *parent, int8 *path)
+{
     Node *n;
     int16 size;
 
@@ -30,7 +31,7 @@ Node *createNode(Node *parent, int8 *path){
 
     size = sizeof(struct s_node);
     n = (Node *)malloc((int)size);
-    zero((int8*)n, size);
+    zero((int8 *)n, size);
 
     parent->west = n;
     n->tag = TagNode;
@@ -40,20 +41,23 @@ Node *createNode(Node *parent, int8 *path){
     return n;
 }
 
-Leaf *findLastLinear(Node *parent){
+Leaf *findLastLinear(Node *parent)
+{
     Leaf *l;
-    
+
     errno = NoError;
     assert(parent);
-    if(!parent->east)
+    if (!parent->east)
         reterr(NoError);
-    for(l = parent->east; l->east; l = l->east);
+    for (l = parent->east; l->east; l = l->east)
+        ;
     assert(l);
 
     return l;
 }
 
-Leaf *createLeaf(Node *parent, int8 *key, int8 *value, int16 count){
+Leaf *createLeaf(Node *parent, int8 *key, int8 *value, int16 count)
+{
     Leaf *l, *new;
     int16 size;
 
@@ -61,32 +65,31 @@ Leaf *createLeaf(Node *parent, int8 *key, int8 *value, int16 count){
     l = findLastLinear(parent);
 
     size = sizeof(struct s_leaf);
-    new = (Leaf*)malloc(size);
+    new = (Leaf *)malloc(size);
     assert(new);
 
-    if(!l)
+    if (!l)
         parent->east = new;
     else
         l->east = new;
 
     zero((int8 *)new, size);
     new->tag = TagLeaf;
-    new->west = (!l) ? 
-        (Tree *)parent : 
-        (Tree *)l;
+    new->west = (!l) ? (Tree *)parent : (Tree *)l;
 
-    strncpy((char*)new->key, (char *)key, 127);
+    strncpy((char *)new->key, (char *)key, 127);
     new->value = (int8 *)malloc(count);
     zero(new->value, count);
 
     assert(new->value);
-    strncpy((char*)new->value, (char*)value, count);
+    strncpy((char *)new->value, (char *)value, count);
     new->size = count;
 
-    return l;
+    return new;
 }
 
-int main(){
+int main()
+{
     Node *n, *n2;
     Leaf *l1, *l2;
     int8 *key, *value;
@@ -96,20 +99,28 @@ int main(){
     assert(n);
 
     n2 = createNode((Node *)&root, (int8 *)"/Users/Login");
-	assert(n);
+    assert(n);
 
     key = (int8 *)"user";
     value = (int8 *)"abc77301aa";
-    size = (int16) strlen((char*)value);
+    size = (int16)strlen((char *)value);
     l1 = createLeaf(n2, key, value, size);
     assert(l1);
 
     printf("%s\n", l1->value);
 
+    key = "john";
+    value = "aa034945c";
+    size = (int16)strlen((char *)value);
+    l2 = createLeaf(n2, key, value, size);
+    assert(l2);
+
+    printf("%s\n", l2->key);
+
     printf("%p %p\n", n, n2);
 
     free(n2);
     free(n);
-    
+
     return 0;
 }
